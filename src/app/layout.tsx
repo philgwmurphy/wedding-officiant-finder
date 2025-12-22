be1aa693327c590e60acd6fc8ed3f0a99f387cd0
philgwmurphy <phillipgwmurphy@gmail.com>
2025-12-22 16:44:09 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react"; // 1. Import the component
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -30,7 +31,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        {children}
+        <Analytics /> {/* 2. Add the component here */}
+      </body>
     </html>
   );
 }
